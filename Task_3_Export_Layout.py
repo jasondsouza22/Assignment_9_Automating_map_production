78bcc01c5bbd118ef863fc62662730c2e98b98b7
jasondsouza22 <jasondsouza2211@gmail.com>
2023-11-07 22:59:57 +0530

    Export_Layout

---
+++ Task_3_Export_Layout.py
@@ -0,0 +1,15 @@
+import arcpy
+
+pro_project_path = r"D:\Second Year\Sem 3\Programming_for_GIS_III\Assignments\Assignment_9_Automating_map_production\Assignment_9_ProProject\MyProject.aprx"
+
+my_project = arcpy.mp.ArcGISProject(pro_project_path)
+
+layout_list = my_project.listLayouts()
+
+for layout in layout_list:
+    print(layout.name)
+
+    layout.exportToPNG(r"D:\Second Year\Sem 3\Programming_for_GIS_III\Assignments\Assignment_9_Automating_map_production\Output\Wilson_Map_Layout.png")
+
+print("Layout Exported")
+
